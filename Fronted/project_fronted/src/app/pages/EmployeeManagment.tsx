aff6e002d8ae6ccadfb1a8c65bda1e608476b8f2
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-03-17 21:40:20 +0200

    initial project

---
+++ Fronted/project_fronted/src/app/pages/EmployeeManagment.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeManagment = () => {
+  return (
+    <div>
+      <h1>Employee Managment</h1>
+    </div>
+  )
+}
+
+export default EmployeeManagment
---
+++ Fronted/project_fronted/src/app/pages/Tasks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tasks = () => {
+  return (
+    <div>
+      <h1>task</h1>
+    </div>
+  )
+}
+
+export default Tasks
