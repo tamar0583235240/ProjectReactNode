058e2016e06758457694aef5d2e5a6b93c3642f0
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-03-21 01:26:48 +0200

    add routes status

---
+++ Backend/routes/statusRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { AddStatus, AllStatuses, DeleteStatus, UpdateStatus } = require('../controllers/statusesController');
+
+router.post('/AllStatuses', AllStatuses);
+router.post('/AddStatus', AddStatus);
+router.delete('/DeleteStatus/:status_id', DeleteStatus);
+router.put('/UpdateStatus/:status_id', UpdateStatus);
+
+module.exports = router;
