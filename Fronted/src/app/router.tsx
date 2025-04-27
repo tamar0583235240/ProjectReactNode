513a1ff0e01950c0f98a2060d46f55ce0a211a92
ayalacohen786 <ac33116878@gmail.com>
2025-04-27 16:24:40 +0300

    update Landing Page

--- Fronted/src/app/router.tsx
+++ Fronted/src/app/router.tsx
@@ -4,12 +4,13 @@ import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
 import EmployeeManagment from "./pages/EmployeeManagment";
 import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout />,
         children: [
-            { element: <HomePage />, index: true },
+            { element: <LandingPage/>, index: true },
             { path: "Projects", element: <Projects /> },
             { path: "Tasks", element: <Tasks /> },
             { path: "EmployeeManagment", element: <EmployeeManagment /> },
