d6233ff01a376eff91c55be9d3ff5cccacd279b3
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-04-29 00:39:05 +0300

    add prafix api to the route

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const tasksRoutes = require('./routes/tasksRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 const organizationsRoutes = require('./routes/organizationsRoutes');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7001;
 const app = express();
 
 connectDB();
@@ -23,18 +23,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-app.use("/projects", projectsRoutes);
-app.use("/roles", rolesRoutes);
-app.use("/statuses", statusesRoutes);
-app.use("/tasks", tasksRoutes);
-// app.use("/users", usersRoutes);
-// app.use("/organizations", organizationsRoutes);
+app.use("/api/projects", projectsRoutes);
+app.use("/api/roles", rolesRoutes);
+app.use("/api/statuses", statusesRoutes);
+app.use("/api/tasks", tasksRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/organizations", organizationsRoutes);
 
-
 // Home route
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("this is the home page");
 });
 
