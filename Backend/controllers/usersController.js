7b2e4d31a46dcd8922cb7c48728e812609ed47d8
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-05-01 01:55:29 +0300

    change location of function

--- Backend/controllers/usersController.js
+++ Backend/controllers/usersController.js
@@ -1,32 +1,32 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt')
 
-exports.signUp = async (req, res) => {
-    try {
-        const { user_name, password, email, role, manager_id, organization_id } = req.body
-        if (!user_name || !password) {
-            return res.status(400).json({ message: 'All fields are required' })
-        }
-        const duplicate = await User.findOne({ user_name: user_name }).lean()
-        if (duplicate) {
-            return res.status(409).json({ message: "Duplicate username" })
-        }
-        const hashedPwd = await bcrypt.hash(password, 10)
-        const userObject = { user_name, password: hashedPwd, email, role, manager_id, organization_id }
-        const user = await User.create(userObject)
-        if (user) {
-            return res.status(201).json(user,{
-                message: `New user ${user.user_name}
-            created` })
+// exports.signUp = async (req, res) => {
+//     try {
+//         const { user_name, password, email, role, manager_id, organization_id } = req.body
+//         if (!user_name || !password) {
+//             return res.status(400).json({ message: 'All fields are required' })
+//         }
+//         const duplicate = await User.findOne({ user_name: user_name }).lean()
+//         if (duplicate) {
+//             return res.status(409).json({ message: "Duplicate username" })
+//         }
+//         const hashedPwd = await bcrypt.hash(password, 10)
+//         const userObject = { user_name, password: hashedPwd, email, role, manager_id, organization_id }
+//         const user = await User.create(userObject)
+//         if (user) {
+//             return res.status(201).json(user,{
+//                 message: `New user ${user.user_name}
+//             created` })
             
-        } else {
-            return res.status(400).json({ message: 'Invalid user received' })
-        }
-    } catch (error) {
-        console.error('Failed to add user:', error);
-        res.status(500).json({ message: 'Failed to add user', error: error.message });
-    }
-};
+//         } else {
+//             return res.status(400).json({ message: 'Invalid user received' })
+//         }
+//     } catch (error) {
+//         console.error('Failed to add user:', error);
+//         res.status(500).json({ message: 'Failed to add user', error: error.message });
+//     }
+// };
 
 exports.AllUsers = async (req, res) => {
     try {
@@ -81,22 +81,22 @@ exports.UpdateUser = async (req, res) => {
     }
 };
 
-exports.signIn = async (req, res) => {
-    const { user_name, password } = req.body
-    if (!user_name || !password) {
-        return res.status(400).json({message:'All fields are required'})
-        }
-    try {
-        const user = await User.findOne({user_name}).lean()
-        if (!user||user.active) {
-            return res.status(401).json({ message: 'Unauthorized' })
-        }
-       const match = await bcrypt.compare(password, user.password);
-       if(!match)return res.status(401).json({message:'Unauthorized'})
-        res.json(user)
-    }
-    catch{
-              return res.status(404).json({message:'user not found'})
-    }
+// exports.signIn = async (req, res) => {
+//     const { user_name, password } = req.body
+//     if (!user_name || !password) {
+//         return res.status(400).json({message:'All fields are required'})
+//         }
+//     try {
+//         const user = await User.findOne({user_name}).lean()
+//         if (!user||user.active) {
+//             return res.status(401).json({ message: 'Unauthorized' })
+//         }
+//        const match = await bcrypt.compare(password, user.password);
+//        if(!match)return res.status(401).json({message:'Unauthorized'})
+//         res.json(user)
+//     }
+//     catch{
+//               return res.status(404).json({message:'user not found'})
+//     }
     
-};
+// };
