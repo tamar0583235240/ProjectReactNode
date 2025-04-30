3b3b2df3e2332a0d1f4b9c9bdaf092f11eff1f71
ayalacohen786 <ac33116878@gmail.com>
2025-05-01 02:43:19 +0300

    update api

--- Fronted/src/app/api.ts
+++ Fronted/src/app/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ 
-    baseUrl: "http://localhost:5000/api/" }),
+    baseUrl: "http://localhost:5000" }),
   tagTypes: ["User","Organization", "Project", "Task"],
   endpoints: () => ({}),
 });
