7d4ac71f2b3ecdae99da203dfedfe6bcde8e2ee9
ayalacohen786 <ac33116878@gmail.com>
2025-05-01 04:57:25 +0300

    add Token

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User")
 const bcrypt = require('bcrypt');
+const jwt= require('jsonwebtoken')
 
 exports.SignUp = async (req, res) => {
     try {
@@ -7,7 +8,7 @@ exports.SignUp = async (req, res) => {
         if (!user_name || !password || !email || !role || !organization_id) {
             return res.status(400).json({ message: 'All fields are required' });
         }
-        
+
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: 'This email already exists in the system' });
@@ -20,7 +21,7 @@ exports.SignUp = async (req, res) => {
                 user,
                 message: `New user ${user.user_name} created`
             });
-            
+
 
         } else {
             return res.status(400).json({ message: 'Invalid user received' })
@@ -31,22 +32,39 @@ exports.SignUp = async (req, res) => {
     }
 };
 exports.SignIn = async (req, res) => {
-    const { user_name, password } = req.body
-    if (!user_name || !password) {
-        return res.status(400).json({ message: 'All fields are required' })
-    }
     try {
-        const user = await User.findOne({ user_name }).lean()
-        if (!user || user.active) {
-            return res.status(401).json({ message: 'Unauthorized' })
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
         }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
         const match = await bcrypt.compare(password, user.password);
-        if (!match) return res.status(401).json({ message: 'Unauthorized' })
-        res.json(user)
-    }
-    catch {
-        return res.status(404).json({ message: 'user not found' })
+        if (!match) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+        const  accessToken = jwt.sign(
+            {
+              userId: user._id,
+              role: user.role,
+              organization_id: user.organization_id,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1h' }
+
+          );
+          res.json({accessToken:accessToken})
+
+    } catch (err) {
+        console.error('Login error:', err);
+        res.status(500).json({ message: 'Login failed', error: err.message });
     }
 
 }
+
 
