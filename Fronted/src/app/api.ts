a8eaf5c9ad3fbf067a2e3c4f46b219f4d6459c04
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-05-01 03:06:56 +0300

    change api

--- Fronted/src/app/api.ts
+++ Fronted/src/app/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ 
-    baseUrl: "http://localhost:5000" }),
+    baseUrl: "http://localhost:5000/api/" }),
   tagTypes: ["User","Organization", "Project", "Task"],
   endpoints: () => ({}),
 });
